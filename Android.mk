2d483b77c293a3ee7eaa9bc72a04cc57268e51dc
Thanh Dat Nguyen <thanhdatppro@gmail.com>
2019-07-19 13:25:46 +0700

    dasd

--- Android.mk
+++ Android.mk
@@ -79,6 +79,27 @@ LOCAL_MODULE_CLASS := SHARED_LIBRARIES
 LOCAL_MODULE_SUFFIX := .so
 include $(BUILD_PREBUILT)
 
+include $(CLEAR_VARS)
+LOCAL_MODULE := libjson
+LOCAL_MODULE_OWNER := meizu
+LOCAL_SRC_FILES_64 := proprietary/lib64/libjson.so
+LOCAL_SRC_FILES_32 := proprietary/lib/libjson.so
+LOCAL_MULTILIB := both
+LOCAL_MODULE_TAGS := optional
+LOCAL_MODULE_CLASS := SHARED_LIBRARIES
+LOCAL_MODULE_SUFFIX := .so
+include $(BUILD_PREBUILT)
+
+
+
+
+
+
+
+
+
+
+
 
 
 
--- bicot-vendor.mk
+++ bicot-vendor.mk
@@ -16,6 +16,7 @@ PRODUCT_COPY_FILES += \
 
 PRODUCT_PACKAGES += \
     libqdMetaData \
+    libjson \
     libqsap_sdk \
     libQWiFiSoftApCfg \
     libqdutils \
