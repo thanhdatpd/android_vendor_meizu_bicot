ac9afd018e12b86e1d0d382c222e139b09fb6d95
thanhdatpd <thanhdatppro@gmail.com>
2019-07-22 18:28:53 +0700

    Update Android.mk

--- Android.mk
+++ Android.mk
@@ -128,16 +128,16 @@ include $(BUILD_PREBUILT)
 #LOCAL_MODULE_SUFFIX := .so
 #include $(BUILD_PREBUILT)
 
-include $(CLEAR_VARS)
-LOCAL_MODULE := libinputflinger
-LOCAL_MODULE_OWNER := meizu
-LOCAL_SRC_FILES_64 := proprietary/lib64/libinputflinger.so
-LOCAL_SRC_FILES_32 := proprietary/lib/libinputflinger.so
-LOCAL_MULTILIB := both
-LOCAL_MODULE_TAGS := optional
-LOCAL_MODULE_CLASS := SHARED_LIBRARIES
-LOCAL_MODULE_SUFFIX := .so
-include $(BUILD_PREBUILT)
+#include $(CLEAR_VARS)
+#LOCAL_MODULE := libinputflinger
+#LOCAL_MODULE_OWNER := meizu
+#LOCAL_SRC_FILES_64 := proprietary/lib64/libinputflinger.so
+#LOCAL_SRC_FILES_32 := proprietary/lib/libinputflinger.so
+#LOCAL_MULTILIB := both
+#LOCAL_MODULE_TAGS := optional
+#LOCAL_MODULE_CLASS := SHARED_LIBRARIES
+#LOCAL_MODULE_SUFFIX := .so
+#include $(BUILD_PREBUILT)
 
 #include $(CLEAR_VARS)
 #LOCAL_MODULE := libpowermanager
