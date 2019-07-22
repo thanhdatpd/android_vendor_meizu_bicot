6b5861d6e36a43a188fdcc2dc50523933b10ab56
thanhdatpd <thanhdatppro@gmail.com>
2019-07-22 11:45:10 +0700

    Update Android.mk

--- Android.mk
+++ Android.mk
@@ -139,27 +139,27 @@ LOCAL_MODULE_CLASS := SHARED_LIBRARIES
 LOCAL_MODULE_SUFFIX := .so
 include $(BUILD_PREBUILT)
 
-include $(CLEAR_VARS)
-LOCAL_MODULE := libpowermanager
-LOCAL_MODULE_OWNER := meizu
-LOCAL_SRC_FILES_64 := proprietary/lib64/libpowermanager.so
-LOCAL_SRC_FILES_32 := proprietary/lib/libpowermanager.so
-LOCAL_MULTILIB := both
-LOCAL_MODULE_TAGS := optional
-LOCAL_MODULE_CLASS := SHARED_LIBRARIES
-LOCAL_MODULE_SUFFIX := .so
-include $(BUILD_PREBUILT)
+#include $(CLEAR_VARS)
+#LOCAL_MODULE := libpowermanager
+#LOCAL_MODULE_OWNER := meizu
+#LOCAL_SRC_FILES_64 := proprietary/lib64/libpowermanager.so
+#LOCAL_SRC_FILES_32 := proprietary/lib/libpowermanager.so
+#LOCAL_MULTILIB := both
+#LOCAL_MODULE_TAGS := optional
+#LOCAL_MODULE_CLASS := SHARED_LIBRARIES
+#LOCAL_MODULE_SUFFIX := .so
+3include $(BUILD_PREBUILT)
 
-include $(CLEAR_VARS)
-LOCAL_MODULE := libpower
-LOCAL_MODULE_OWNER := meizu
-LOCAL_SRC_FILES_64 := proprietary/lib64/libpower.so
-LOCAL_SRC_FILES_32 := proprietary/lib/libpower.so
-LOCAL_MULTILIB := both
-LOCAL_MODULE_TAGS := optional
-LOCAL_MODULE_CLASS := SHARED_LIBRARIES
-LOCAL_MODULE_SUFFIX := .so
-include $(BUILD_PREBUILT)
+#include $(CLEAR_VARS)
+3LOCAL_MODULE := libpower
+#LOCAL_MODULE_OWNER := meizu
+#LOCAL_SRC_FILES_64 := proprietary/lib64/libpower.so
+#LOCAL_SRC_FILES_32 := proprietary/lib/libpower.so
+3LOCAL_MULTILIB := both
+#LOCAL_MODULE_TAGS := optional
+#LOCAL_MODULE_CLASS := SHARED_LIBRARIES
+#LOCAL_MODULE_SUFFIX := .so
+#include $(BUILD_PREBUILT)
 
 include $(CLEAR_VARS)
 LOCAL_MODULE := libwifi-hal-qcom
