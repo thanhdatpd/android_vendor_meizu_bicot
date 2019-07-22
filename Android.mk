211a84b8efd705c9f6cc9b79cfe0ba34c5d0f3bb
thanhdatpd <thanhdatppro@gmail.com>
2019-07-22 16:56:32 +0700

    Update Android.mk

--- Android.mk
+++ Android.mk
@@ -150,16 +150,16 @@ include $(BUILD_PREBUILT)
 #LOCAL_MODULE_SUFFIX := .so
 #include $(BUILD_PREBUILT)
 
-#include $(CLEAR_VARS)
-#LOCAL_MODULE := libpower
-#LOCAL_MODULE_OWNER := meizu
-#LOCAL_SRC_FILES_64 := proprietary/lib64/libpower.so
-#LOCAL_SRC_FILES_32 := proprietary/lib/libpower.so
-#LOCAL_MULTILIB := both
-#LOCAL_MODULE_TAGS := optional
-#LOCAL_MODULE_CLASS := SHARED_LIBRARIES
-#LOCAL_MODULE_SUFFIX := .so
-#include $(BUILD_PREBUILT)
+include $(CLEAR_VARS)
+LOCAL_MODULE := libmemalloc
+LOCAL_MODULE_OWNER := meizu
+LOCAL_SRC_FILES_64 := proprietary/lib64/libmemalloc.so
+LOCAL_SRC_FILES_32 := proprietary/lib/libmemalloc.so
+LOCAL_MULTILIB := both
+LOCAL_MODULE_TAGS := optional
+LOCAL_MODULE_CLASS := SHARED_LIBRARIES
+LOCAL_MODULE_SUFFIX := .so
+include $(BUILD_PREBUILT)
 
 include $(CLEAR_VARS)
 LOCAL_MODULE := libwifi-hal-qcom
