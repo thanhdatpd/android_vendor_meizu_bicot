18017eb82d36339913cbc151db78a3e7373ee973
thanhdatpd <thanhdatppro@gmail.com>
2019-08-04 06:39:19 +0700

    Update Android.mk

--- Android.mk
+++ Android.mk
@@ -207,7 +207,17 @@ include $(BUILD_PREBUILT)
 LOCAL_SDK_VERSION := 25
 ifeq ($(TARGET_DEVICE),bicot)
 
-
+include $(CLEAR_VARS)
+LOCAL_MODULE := libtime_genoff
+LOCAL_MODULE_OWNER := meizu
+LOCAL_SRC_FILES_64 := proprietary/vendor/lib64/libtime_genoff.so
+LOCAL_SRC_FILES_32 := proprietary/vendor/lib/libtime_genoff.so
+LOCAL_MULTILIB := both
+LOCAL_MODULE_TAGS := optional
+LOCAL_MODULE_CLASS := SHARED_LIBRARIES
+LOCAL_MODULE_SUFFIX := .so
+LOCAL_PROPRIETARY_MODULE := true
+include $(BUILD_PREBUILT)
 
 
 include $(CLEAR_VARS)
